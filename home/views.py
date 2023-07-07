86416a73201b9b7759a3d3f75227a9cc20796ea9
Oscardk <dauren2050@yandex.ru>
2023-07-07 16:25:56 +0600

    Sende_V1

--- home/views.py
+++ home/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+def transfer_view(request):
+    if request.method == 'POST':
+        # Обработка данных формы перевода средств
+        sender = request.POST['sender']
+        recipient = request.POST['recipient']
+        amount = request.POST['amount']
+
+        # Ваш код для выполнения перевода средств
+
+        # Отправка данных в шаблон
+        context = {
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+            # Дополнительные данные, которые вы хотите передать в шаблон
+        }
+
+        return render(request, 'transfer_success.html', context)
+    else:
+        return render(request, 'index.html')
